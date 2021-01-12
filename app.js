11af3b507ca026f8321bd592e814624310669c53
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-11 20:53:05 -0800

    Save current user as res.locals.currentUser

--- app.js
+++ app.js
@@ -58,6 +58,10 @@ passport.deserializeUser(function (id, done) {
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('index', { user: req.user });
